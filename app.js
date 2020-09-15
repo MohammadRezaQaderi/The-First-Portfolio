23a7901f42009073b88a2e1965da0f9eccb48f9b
MohammadRezaQaderi <mrq112775@outlook.com>
2020-09-15 09:36:41 +0430

    the document event listener scroll

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position >450){
+        header.style.backgroundColor = '#29323c'
+    }else{
+        header.style.backgroundColor = 'transparent'
+    }
+})
+
